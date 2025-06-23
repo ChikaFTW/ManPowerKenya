972b0dac7a80d8766e88ff9658cdb2970e3081ab
ChikaFTW <salim.7777@live.fr>
2025-06-23 04:42:11 +0100

    Adding translation to  the core

---
+++ assets/translations/translations.js
@@ -0,0 +1,247 @@
+const translations = {
+  "en": {
+    "header_title": "Job Hunt",
+    "welcome": "we are ready to help you",
+    "guard_services": "GUARD SERVICES",
+    "right_people": "RIGHT PEOPLE IN THE RIGHT PLACE",
+    "our_services": "our services",
+    "support": "we are here to support you",
+    "security_control": "SECURITY CONTROL & ROOM OPERATOR",
+    "house_clean": "we are here to keep your house clean",
+    "housemaid": "HOUSEMAID & Cleaners",
+    "nurses": "NURSES",
+    "drivers": "DRIVERS",
+    "request_call": "Request a call back right now?",
+    "request_text": "We offer the best job opportunities...",
+    "contact_us": "Contact Us",
+    "message_contact_us": "feel free to send us a message now",
+    "work_times":"Mon-Fri 09:00-17:00",
+    "phone_number": "",
+    "base_country": "",
+    "home_nav":"",
+    "about_us_nav":"",
+    "services_nav":"",
+    "contact_us":"",
+    "onepage_nav":"",
+    "requestacallbackrightnow": "Request a call back right now?",
+    "Weofferthebestjobopportunitiesandweare": "We offer the best job opportunities and we are always available for your questions",
+    "contact_us_button_body": "Contact Us",
+    
+    // Services Section
+    "manpowerservices": "Manpower & Consultancy Services",
+    "your_success partner": "your success partner",
+    
+    // Security Service
+    "security_service": "Security",
+    "security_service_1": "Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.",
+    "security_service_2": "Read More",
+    
+    // Housekeeping Service
+    "housekeeping_service": "Housekeeping",
+    "housekeeping_service_1": "Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.",
+    "housekeeping_service_2": "Read More",
+    
+    // Healthcare Service
+    "healthcare_service": "Healthcare",
+    "healthcare_service_1": "Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.",
+    "healthcare_service_2": "Read More",
+    
+    // Transportation Service
+    "transportation_service": "Transportation",
+    "transportation_service_1": "Sed tincidunt dictum lobortis. Aenean tempus diam vel augue luctus dignissim. Nunc ornare leo tortor.",
+    "transportation_service_3": "Read More",
+    "welcome": "we are ready to help you",
+    "guard_services": "GUARD SERVICES",
+    "right_people": "RIGHT PEOPLE IN THE RIGHT PLACE",
+    "our_services": "our services",
+    
+    // Slider 2 (Security Control)
+    "slider_2_1": "we are here to support you",
+    "slider_2_2": "SECURITY CONTROL & ROOM OPERATOR",
+    "slider_2_3": "There may be several duties and responsibilities of security control room operator",
+    "slider_2_4": "our services",
+    
+    // Slider 3 (Housemaid)
+    "slider_3_1": "we are here to keep your house clean",
+    "slider_3_2": "HOUSEMAID & Cleaners",
+    "slider_3_3": "Our Cleaners maintain all facilities",
+    "slider_3_4": "our services",
+    
+    // Slider 4 (Nurses)
+    "slider_4_1": "we are ready to take care of you",
+    "slider_4_2": "NURSES",
+    "slider_4_3": "Working in the healthcare industry can be a challenging, but extremely rewarding career",
+    "slider_4_4": "our services",
+    
+    // Slider 5 (Drivers)
+    "slider_5_1": "we provide skilled drivers",
+    "slider_5_2": "DRIVERS",
+    "slider_5_3": "There are at least two different sets of driving skills that make someone a good driver: technical skills and safety skills",
+    "slider_5_4": "our services"
+  },
+  "fr": {
+    "header_title": "Chasseur d'Emploi",
+    "welcome": "nous sommes prêts à vous aider",
+    "guard_services": "SERVICES DE GARDE",
+    "right_people": "LES BONNES PERSONNES AU BON ENDROIT",
+    "our_services": "nos services",
+    "support": "nous sommes là pour vous soutenir",
+    "security_control": "CONTRÔLE DE SÉCURITÉ & OPÉRATEUR",
+    "house_clean": "nous sommes là pour garder votre maison propre",
+    "housemaid": "FEMME DE MÉNAGE & Nettoyeurs",
+    "nurses": "INFIRMIÈRES",
+    "drivers": "CHAUFFEURS",
+    "request_call": "Demander un rappel maintenant?",
+    "request_text": "Nous offrons les meilleures opportunités...",
+    "contact_us": "Contactez-Nous",
+    "message_contact_us": "hésitez pas a nous envoyer un message maintenant" ,
+    "work_times":"Lun-Ven 09:00-17:00",
+    "phone_number":"+ 254 703-952-223",
+    "base_country":"République du Kenya",
+  "home_nav": "Accueil",
+    "about_us_nav": "À propos",
+    "services_nav": "Nos services",
+    "contact_us": "Contactez-nous",
+    "onepage_nav": "Page unique",
+   "requestacallbackrightnow": "Demander un rappel maintenant?",
+    "Weofferthebestjobopportunitiesandweare": "Nous offrons les meilleures opportunités d'emploi et nous sommes toujours disponibles pour vos questions",
+    "contact_us_button_body": "Contactez-nous",
+    "requestacallbackrightnow": "Demander un rappel maintenant?",
+    "Weofferthebestjobopportunitiesandweare": "Nous offrons les meilleures opportunités d'emploi et nous sommes toujours disponibles pour vos questions",
+    "contact_us_button_body": "Contactez-nous",
+    
+    // Services Section
+    "manpowerservices": "Services de main-d'œuvre et de conseil",
+    "your_success partner": "votre partenaire de succès",
+    
+    // Security Service
+    "security_service": "Sécurité",
+    "security_service_1": "Nous fournissons des services de sécurité complets pour protéger vos biens et votre personnel selon les normes les plus élevées.",
+    "security_service_2": "Lire la suite",
+    
+    // Housekeeping Service
+    "housekeeping_service": "Ménage",
+    "housekeeping_service_1": "Notre équipe qualifiée propose des services de nettoyage complets pour les maisons et les bureaux à des prix compétitifs.",
+    "housekeeping_service_2": "Lire la suite",
+    
+    // Healthcare Service
+    "healthcare_service": "Soins de santé",
+    "healthcare_service_1": "Nous fournissons des infirmières et des soignants qualifiés pour offrir les meilleurs services de santé à domicile.",
+    "healthcare_service_2": "Lire la suite",
+    
+    // Transportation Service
+    "transportation_service": "Transport",
+    "transportation_service_1": "Des chauffeurs professionnels avec des véhicules modernes pour assurer des déplacements sûrs et confortables pour tous vos besoins.",
+    "transportation_service_3": "Lire la suite",
+    "welcome": "nous sommes prêts à vous aider",
+    "guard_services": "SERVICES DE GARDE",
+    "right_people": "LES BONNES PERSONNES AU BON ENDROIT",
+    "our_services": "nos services",
+    
+    // Slider 2 (Security Control)
+    "slider_2_1": "nous sommes là pour vous soutenir",
+    "slider_2_2": "CONTRÔLE DE SÉCURITÉ & OPÉRATEUR",
+    "slider_2_3": "Il peut y avoir plusieurs tâches et responsabilités pour un opérateur de salle de contrôle de sécurité",
+    "slider_2_4": "nos services",
+    
+    // Slider 3 (Housemaid)
+    "slider_3_1": "nous sommes là pour garder votre maison propre",
+    "slider_3_2": "FEMME DE MÉNAGE & Nettoyeurs",
+    "slider_3_3": "Nos nettoyeurs entretiennent toutes les installations",
+    "slider_3_4": "nos services",
+    
+    // Slider 4 (Nurses)
+    "slider_4_1": "nous sommes prêts à prendre soin de vous",
+    "slider_4_2": "INFIRMIÈRES",
+    "slider_4_3": "Travailler dans le secteur de la santé peut être un défi, mais c'est une carrière extrêmement gratifiante",
+    "slider_4_4": "nos services",
+    
+    // Slider 5 (Drivers)
+    "slider_5_1": "nous fournissons des chauffeurs qualifiés",
+    "slider_5_2": "CHAUFFEURS",
+    "slider_5_3": "Il y a au moins deux ensembles différents de compétences de conduite qui font un bon chauffeur : compétences techniques et compétences de sécurité",
+    "slider_5_4": "nos services"
+    
+    
+  },
+  "ar": {
+    "header_title": "بحث عن عمل",
+    "welcome": "نحن على استعداد لمساعدتك",
+    "guard_services": "خدمات الحراسة",
+    "right_people": "الشخص المناسب في المكان المناسب",
+    "our_services": "خدماتنا",
+    "support": "نحن هنا لدعمك",
+    "security_control": "التحكم في الأمن ومشغلو الغرف",
+    "house_clean": "نحن هنا للحفاظ على نظافة منزلك",
+    "housemaid": "خادمات ومنظفات",
+    "nurses": "ممرضات",
+    "drivers": "سائقون",
+    "request_call": "طلب اتصال الآن؟",
+    "request_text": "نحن نقدم أفضل فرص العمل...",
+    "contact_us": "اتصل بنا",
+    "message_contact_us": "لا تتردد في إرسال رسالة لنا الآن" ,
+    "work_times": "من الإثنين إلى الجمعة 09:00-17:00",
+    "phone_number":" 703-952-223 254 +", 
+    "base_country":"جمهورية كينيا",
+     "home_nav": "الرئيسية",
+    "about_us_nav": "من نحن",
+    "services_nav": "خدماتنا",
+    "contact_us": "اتصل بنا",
+    "onepage_nav": "صفحة واحدة",
+    "requestacallbackrightnow": "طلب اتصال الآن؟",
+    "Weofferthebestjobopportunitiesandweare": "نحن نقدم أفضل فرص العمل ونحن دائما متاحون لأسئلتك",
+    "contact_us_button_body": "اتصل بنا",
+    
+    // Services Section
+    "manpowerservices": "خدمات القوى العاملة والاستشارات",
+    "your_success partner": "شريك نجاحك",
+    
+    // Security Service
+    "security_service": "الأمن",
+    "security_service_1": "نحن نقدم خدمات أمنية متكاملة لحماية ممتلكاتك وأفرادك بأعلى معايير الجودة.",
+    "security_service_2": "اقرأ المزيد",
+    
+    // Housekeeping Service
+    "housekeeping_service": "خدمات النظافة",
+    "housekeeping_service_1": "فريقنا المدرب يقدم خدمات تنظيف شاملة للمنازل والمكاتب بأسعار تنافسية.",
+    "housekeeping_service_2": "اقرأ المزيد",
+    
+    // Healthcare Service
+    "healthcare_service": "الرعاية الصحية",
+    "healthcare_service_1": "نوفر ممرضات ومقدمي رعاية مؤهلين لتقديم أفضل الخدمات الصحية في منزلك.",
+    "healthcare_service_2": "اقرأ المزيد",
+    
+    // Transportation Service
+    "transportation_service": "خدمات النقل",
+    "transportation_service_1": "سائقون محترفون بسيارات حديثة لضمان رحلات آمنة ومريحة لجميع احتياجاتك.",
+    "transportation_service_3": "اقرأ المزيد",
+    "welcome": "نحن على استعداد لمساعدتك",
+    "guard_services": "خدمات الحراسة",
+    "right_people": "الشخص المناسب في المكان المناسب",
+    "our_services": "خدماتنا",
+    
+    // Slider 2 (Security Control)
+    "slider_2_1": "نحن هنا لدعمك",
+    "slider_2_2": "التحكم في الأمن ومشغلو الغرف",
+    "slider_2_3": "قد تكون هناك عدة واجبات ومسؤوليات لمشغل غرفة التحكم الأمنية",
+    "slider_2_4": "خدماتنا",
+    
+    // Slider 3 (Housemaid)
+    "slider_3_1": "نحن هنا للحفاظ على نظافة منزلك",
+    "slider_3_2": "خادمات ومنظفات",
+    "slider_3_3": "يقوم منظفونا بصيانة جميع المرافق",
+    "slider_3_4": "خدماتنا",
+    
+    // Slider 4 (Nurses)
+    "slider_4_1": "نحن على استعداد لرعايتك",
+    "slider_4_2": "ممرضات",
+    "slider_4_3": "العمل في مجال الرعاية الصحية قد يكون صعبًا ولكنه مجزٍ للغاية",
+    "slider_4_4": "خدماتنا",
+    
+    // Slider 5 (Drivers)
+    "slider_5_1": "نوفر سائقين ماهرين",
+    "slider_5_2": "سائقون",
+    "slider_5_3": "هناك نوعان على الأقل من مهارات القيادة التي تجعل السائق جيدًا: المهارات الفنية ومهارات السلامة",
+    "slider_5_4": "خدماتنا"
+  }
+};
